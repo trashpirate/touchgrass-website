8fed714e5718fb17af3597e5c159c7d5ed3261e4
trashpirate <nadinaoates@gmail.com>
2024-03-16 15:46:31 -0500

    fix main menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,7 +1,9 @@
+'use client'
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
 import { useState } from "react";
 var classNames = require('classnames');
-
+import BuyButton from '../buttons';
 const navigation = [
     { name: 'Introduction', href: '#introduction', current: true },
     { name: 'Tokenomics', href: '#tokenomics', current: false },
@@ -15,7 +17,8 @@ export default function Menu() {
     const handleClick = () => setClick(!click);
 
     return (
-        <div className="fixed top-0 w-full flex z-30 h-16">
+        <div className="fixed top-0 w-full flex z-30 h-16 my-4 justify-between">
+
             <div className={classNames(click ? "top-0" : "top-full", "flex fixed h-full w-full bg-secondary z-40 transition-all ease-in-out duration-500")}>
                 <div className="flex flex-col mx-auto max-w-96 my-auto space-y-8 text-center">
 
@@ -40,7 +43,35 @@ export default function Menu() {
                 </div>
 
             </div>
-            <nav className="w-full my-4 flex justify-end h-16 px-12 py-4 z-50">
+            <div className=" h-16 w-16  z-50"></div>
+            {click ? <></> :
+                <div className="hidden sm:ml-6 sm:flex m-auto z-50 w-full  justify-center">
+                    <div className="flex space-x-4 h-full align-middle mx-auto">
+                        {navigation.map((item) => (
+                            <div>
+                                <a
+                                    key={item.name}
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? '  text-primary' : 'text-secondary',
+                                        'rounded-md py-2 text-md leading-4 align-middle my-auto font-semibold uppercase group'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                    <div className={classNames(
+                                        item.current ? '  bg-primary rounded-full w-full' : 'bg-secondary w-0 group-hover:w-full ', "rounded-full h-1 mt-1 transition-all duration-500"
+                                    )}></div>
+                                </a>
+
+                                {/* <div className="bg-primary h-1 w-0 hover:w-full px-2"></div> */}
+                            </div>
+                        ))}
+                        <BuyButton></BuyButton>
+                    </div>
+                </div>}
+
+            <nav className="w-16 flex justify-end h-16 px-12 py-4 z-50">
                 <div className="flex align-middle " onClick={handleClick}>
                     <div className="h-10 w-10 my-auto text-primary transition-all ease-in-out duration-500 hover:scale-125">{click ? <XMarkIcon /> : <Bars3Icon />}</div>
                 </div>
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -13,32 +13,7 @@ export default function Navbar() {
     return (
         <nav className="top-0 w-full my-4 flex justify-center h-16 ">
 
-            <div className="hidden sm:ml-6 sm:block my-auto">
-                <div className="flex space-x-4 h-full align-middle">
-                    {navigation.map((item) => (
-                        <div>
-                            <a
-                                key={item.name}
-                                href={item.href}
-                                className={classNames(
-                                    item.current ? '  text-primary' : 'text-secondary',
-                                    'rounded-md py-2 text-md leading-4 align-middle my-auto font-semibold uppercase group'
-                                )}
-                                aria-current={item.current ? 'page' : undefined}
-                            >
-                                {item.name}
-                                <div className={classNames(
-                                    item.current ? '  bg-primary rounded-full w-full' : 'bg-secondary w-0 group-hover:w-full ', "rounded-full h-1 mt-1 transition-all duration-500"
-                                )}></div>
-                            </a>
 
-                            {/* <div className="bg-primary h-1 w-0 hover:w-full px-2"></div> */}
-                        </div>
-                    ))}
-                    <BuyButton></BuyButton>
-                </div>
-
-            </div>
             {/* <div className="h-10 w-10"></div> */}
 
         </nav>
